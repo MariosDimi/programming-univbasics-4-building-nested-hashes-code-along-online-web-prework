17487ba4692573e1d568243c7cdc7095f654dde2
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-26 17:38:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,15 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriach =>
+       :patriach => {
+         :name => "Lord Montague",
+         :age => "53"
+       },
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
+       },
+       :hero
      }
      :capulet => {}
    }
